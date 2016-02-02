86096cd1c5863c5c2459282355ef4a1d02ad614f
Charles Perniciaro <blackbeltcharles@gmail.com>
2016-02-02 00:27:02 -0600

    redid combinations

--- Assets/Scripts/CombatOdds.cs
+++ Assets/Scripts/CombatOdds.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -16,16 +18,15 @@ public class CombatOdds {
 	public double OddsOf(int hits) {
 		// Accumulate a numerator
 		int accumulator = 0;
+		// Create a list for the possible combinations
+		List<bool[]> combinations = new List<bool[]>((int)System.Math.Pow(2, this.Odds.Count));
 		// Set all dice as failing the roll
-		foreach (var odds in Odds) {
-			odds.Active = false;
-		}
-		// Set the first hits number of dice as passing the roll
-		for (int i = 0; i < hits; i++) {
-			this.Odds[i].Active = true;
-		}
+		this.Odds.ForEach (x => x.Active = false);
+		// Generate all combinations that could result in hits
+		this.generateCombinations(hits, combinations);
 		// Iterate the odds for each possible combination that could result in the hits
-		do {
+		for (int i = 0; i < combinations.Count; i++) {
+			this.applyCombination(combinations[i]);
 			// Multiply all the numerators together for each combination of rolls
 			int subAccumulator = 1;
 			foreach (var odds in Odds) {
@@ -33,47 +34,35 @@ public class CombatOdds {
 			}
 			// Add the odds of this combination to the total odds
 			accumulator += subAccumulator;
-		} while(!this.ShiftOdds());
+		}
 		// Divide the accumulated numertors by the number of possible combinations to get the odds of the hits
-		Debug.Log("Numerator " + hits + ": " + accumulator);
-		Debug.Log("Denominator " + hits + ": " + denominator);
+		//Debug.Log("Numerator " + hits + ": " + accumulator);
+		//Debug.Log("Denominator " + hits + ": " + denominator);
 		return accumulator / this.denominator;
 	}
-
-	/*
-	 * The dice are all in a pass or fail state, so the stringified dice might look like this: "1000"
-	 * for one success out of four rolls. The passes get shifted right until all possible combinations are iterated, aka
-	 * 1000 -> 0100 -> 0010 -> 0001 -> done
-	 */ 
-	public bool ShiftOdds() {
-		// Done if all zeroes because there is nothing to shift
-		bool done = this.Odds.All(x => !x.Active);
-		// Check if done (not done if the last bit is inactive and some are active)
-		if (!done && this.Odds.Last().Active) {
-			// Done if all the active are on the right
-			int firstActive = this.Odds.FindIndex(x => x.Active);
-			int lastInactive = this.Odds.FindLastIndex(x => !x.Active);
-			done = lastInactive < firstActive;
-		}
-		// If not done actually shift
-		if (!done) {
-			// Found an inactive one
-			bool found = false;
-			int swap = -1;
-			// The active one furthest right with an inactive one to its right gets shifted to that position
-			int lastInactive = this.Odds.FindLastIndex(x => !x.Active);
-			int lastPossibleActive = lastInactive - 1;
-			int lastActive = this.Odds.FindLastIndex(lastPossibleActive, x => x.Active);
-			for (int i = lastActive; i < this.Odds.Count && !found; i++) {
-				if (!Odds[i].Active) {
-					swap = i;
-					found = true;
-				}
+		
+	// Get all combinations of hits
+	private void generateCombinations(int hits, List<bool[]> combinations) {
+		int power = (int)System.Math.Pow(2, this.Odds.Count);
+		for (int generator = 0; generator < power; generator++) {
+			BitArray bitArray = new BitArray(new int[] { generator });
+			bool[] bits = new bool[bitArray.Count];
+			bitArray.CopyTo(bits, 0);
+			bool[] truncatedBits = new bool[this.Odds.Count];
+			for (int i = 0; i < truncatedBits.Length; i++) {
+				truncatedBits[i] = bits[i];
 			}
-			this.Odds[lastActive].Active = false;
-			this.Odds[swap].Active = true;
+			if (truncatedBits.Select(x => System.Convert.ToInt32(x)).Sum() == hits) {
+				combinations.Add(truncatedBits);
+			}
 		}
-		return done;
+	}
+
+	// Apply the combination to the odds
+	private void applyCombination(bool[] combination) {
+		for (int i = 0; i < combination.Length; i++) {
+			this.Odds[i].Active = combination[i];
+		}
 	}
 
 	public override string ToString() {
